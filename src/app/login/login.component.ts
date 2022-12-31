3fc1b350f7e623a4e0d2f075eb78589fd16b5181
Naruto Uzumaki <ebinsebatian888@gmail.com>
2022-12-31 21:39:17 +0530

    commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  acno:any
+  pass:any
+
+  aim="this is it"
+  phacno="Account Number"
+  phpass="Password"
+
+
+  userDetails:any={
+    1000:{acno:1000,username:"leo",password:"messi",balance:0},
+    1001:{acno:1001,username:"neymar",password:"neymar",balance:0},
+    1002:{acno:1002,username:"andres",password:"iniesta",balance:0},
+    1003:{acno:1003,username:"luis",password:"suarez",balance:0}
+  }
+
+
+  login(){
+    
+    var acno=this.acno
+    var pass=this.pass
+    var userDetails=this.userDetails
+    //the above statement help us to not call each variable by "this" fuction everytime
+
+    if(acno in userDetails){
+      if(pass==userDetails[acno]['password']){
+          alert("Login Success")
+      }
+      else{
+        alert("Incorrect Password")
+      }
+    }
+    else{
+      alert("user not exist")
+    }
+  }
+  acnoChange(event:any){
+    this.acno=event.target.value
+    // console.log(event.target.value)
+  }
+  passChange(event:any){
+    this.pass=event.target.value
+    // console.log(event.target.value)
+  }
+}
