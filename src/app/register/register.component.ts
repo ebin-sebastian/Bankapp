0c0a8b75efea809041916443a6bb173600027a96
Naruto Uzumaki <ebinsebatian888@gmail.com>
2023-01-20 02:44:58 +0530

    validation 1

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,25 +13,36 @@ export class RegisterComponent {
   acno:any
   pass:any
 
-  constructor(private ds:DataService,private router:Router ) { }
+  constructor(private ds:DataService,private router:Router,private formbuilder:FormBuilder ) { }
 
+  //create register form model
 
+  registerForm=this.formbuilder.group({uname:['',[Validators.required,Validators.pattern('[a-zA-Z]+')]],acno:['',[Validators.required,Validators.pattern('[0-9]+')]],pass:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]+')]]})
 
   register(){
 
-      var uname=this.uname
-      var acno= this.acno
-      var pass= this.pass
+      var uname=this.registerForm.value.uname
+      var acno= this.registerForm.value.acno
+      var pass= this.registerForm.value.pass
     
-    const result=this.ds.register(acno,uname,pass)
-    if(result){
-      alert("Registration Success")
-      this.router.navigateByUrl("")
-    }
-    else{
-      alert("Registration Failed")
-    }
+    
 
+    if(this.registerForm.valid){
+      const result=this.ds.register(acno,uname,pass)
+    
+
+          if(result){
+            alert("Registration Success")
+            this.router.navigateByUrl("")
+                    }
+          else{
+              alert("Registration Failed")
+              }
+
+                              }
+  else{
+      alert("form is invalid")
   }
+}
 
 }
